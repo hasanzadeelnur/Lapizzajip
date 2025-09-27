7b95da043f7ebd97221faedc0bd6fcccbfb1695d
Elnur Hasanzade <69572415+hasanzadeelnur@users.noreply.github.com>
2025-09-27 18:45:02 +0400

    fixing bugs

--- Application/Features/Sliders/Profiles/MappingProfiles.cs
+++ Application/Features/Sliders/Profiles/MappingProfiles.cs
@@ -4,13 +4,14 @@ using Domain.Entities;
 using System.Globalization;
 
 namespace Application.Features.Sliders.Profiles;
-internal class MappingProfiles:Profile
+internal class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
-        CreateMap<Slider,GetListSliderDto>()
+        CreateMap<Slider, GetListSliderDto>()
             .ForMember(b => b.Title, m => m.MapFrom(b => b.Translations.FirstOrDefault(b => b.LanguageKey == CultureInfo.CurrentCulture.Name)!.Title))
             .ForMember(b => b.Subtitle, m => m.MapFrom(b => b.Translations.FirstOrDefault(b => b.LanguageKey == CultureInfo.CurrentCulture.Name)!.Subtitle))
             .ReverseMap();
+        CreateMap<SliderImage, GetListSliderImageDto>().ReverseMap();
     }
 }
