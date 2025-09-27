df1845fb334f6b00ee2de991ef91435c13c6f790
Elnur Hasanzade <69572415+hasanzadeelnur@users.noreply.github.com>
2025-09-27 18:37:26 +0400

    fixing bugs

--- Application/Features/Sliders/Queries/GetList/GetListSliderQuery.cs
+++ Application/Features/Sliders/Queries/GetList/GetListSliderQuery.cs
@@ -3,7 +3,6 @@ using AutoMapper;
 using Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System.Globalization;
 
 namespace Application.Features.Sliders.Queries.GetList;
 public class GetListSliderQuery : IRequest<List<GetListSliderDto>>
@@ -13,7 +12,7 @@ public class GetListSliderQuery : IRequest<List<GetListSliderDto>>
     {
         public async Task<List<GetListSliderDto>> Handle(GetListSliderQuery request, CancellationToken cancellationToken)
         {
-            List<Slider> sliders = await _sliderRepository.Query().Where(s => s.Status).OrderBy(s => s.Order).ToListAsync(cancellationToken: cancellationToken);
+            List<Slider> sliders = await _sliderRepository.Query().Include(s => s.Images).Where(s => s.Status).OrderBy(s => s.Order).ToListAsync(cancellationToken: cancellationToken);
 
             List<GetListSliderDto> response = _mapper.Map<List<GetListSliderDto>>(sliders);
 
